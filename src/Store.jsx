bfd791a146d56746086204e64c6eeac3a378f47c
mario-st <mario.st@posteo.de>
2015-11-28 15:51:05 +0100

    comment it

--- src/Store.jsx
+++ src/Store.jsx
@@ -6,12 +6,22 @@ export const EVENT_CHANGE = 'change';
 
 class Store extends EventEmitter {
 
+    /**
+     * initializes an instance of a store
+     * @param {*} dispatcher
+     * @param {Function} register
+     * @param {*} storage
+     */
     constructor (dispatcher, register, storage) {
         super();
         this.stored = storage || {};
         dispatcher.register(register);
     }
 
+    /**
+     * execute change event if something was changed in the store
+     * @param {*} action
+     */
     emitOnChange (action) {
         if (this.changed) {
             this.changed = false;
@@ -19,18 +29,36 @@ class Store extends EventEmitter {
         }
     }
 
+    /**
+     * add event listener on change event
+     * @param {Function} fn
+     */
     onChange (fn) {
         this.on(EVENT_CHANGE, fn);
     }
 
+    /**
+     * remove event listener from change event
+     * @param {Function} fn
+     */
     offChange (fn) {
         this.removeListener(EVENT_CHANGE, fn);
     }
 
+    /**
+     * get an item from the store
+     * @param {String} key
+     * @returns {*}
+     */
     getItem (key) {
         return this.stored[ key ];
     }
 
+    /**
+     * add an item to the store
+     * @param {String} key
+     * @param {*} value
+     */
     setItem (key, value) {
         if (this.stored[ key ] !== value) {
             this.stored[ key ] = value;
@@ -44,12 +72,23 @@ class Store extends EventEmitter {
         }
     }
 
+    /**
+     * append an item to a list inside of the store
+     * @param {String} key
+     * @param {*} value
+     */
     pushItem (key, value) {
         this._createListIfNeeded(key);
         this.stored[ key ].push(value);
         this.changed = true;
     }
 
+    /**
+     * set an item in a list of items by index
+     * @param {String} key
+     * @param {Number} index
+     * @param {*} value
+     */
     setIndexedItem (key, index, value) {
         this._createListIfNeeded(key);
         if (value !== this.stored[ key ][ index ]) {
@@ -58,6 +97,11 @@ class Store extends EventEmitter {
         }
     }
 
+    /**
+     * removes an item from a list of items by index
+     * @param {String} key
+     * @param {*} value
+     */
     delIndexedItem (key, value) {
         this._createListIfNeeded(key);
         let index = this.stored[ key ].indexOf(value);
@@ -67,18 +111,29 @@ class Store extends EventEmitter {
         }
     }
 
+    /**
+     * insert multiple items in the store
+     * @param {Object} obj
+     */
     setItems (obj) {
         var keys = Object.keys(obj).filter((value, key) => this.stored[ key ] !== value, this);
         keys.forEach((key) => this.stored[ key ] = obj[ key ], this);
         this.changed = keys.length > 0;
     }
 
+    /**
+     * remove multiple items in the store
+     * @param {String[]} keys
+     */
     delItems (...keys) {
         keys = keys.filter((key) => this.stored[ key ], this);
         keys.forEach((key) => delete this.stored[ key ], this);
         this.changed = keys.length > 0;
     }
 
+    /**
+     * remove every item in the store
+     */
     cleanUp () {
         this.stored  = {};
         this.changed = true;
